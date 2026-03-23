8f02a89fbcf63e2c2b2edcae1263305116c7f506
倪建乔 <nileonx@gmail.com>
2026-03-23 17:27:47 +0800

    Remove db/ from tracked files (in gitignore)

--- db/migrations/20260321_users_password.sql
+++
@@ -1,5 +0,0 @@
--- Align DB schema with ORM: app.models.Users.password
--- NOTE: MySQL will error if the column already exists; run once.
-
-ALTER TABLE users
-  ADD COLUMN password VARCHAR(16) NOT NULL DEFAULT '123456' COMMENT 'User Password' AFTER uname;
--- db/model_to_orm.ps1
+++
@@ -1,2 +0,0 @@
-# 生成 ORM 模型到文件
-sqlacodegen mysql://localhost:3308/chatmooc --outfile models.py
